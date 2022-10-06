303c74d2b250a5017d3bc5d2bdc65392f6e9f546
imisiaina <imis.aina1@gmail.com>
2022-10-06 13:48:24 +0100

    Updated Heroku Database url

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -12,21 +12,21 @@ class DatabaseConnection
   # the database name given in argument.
   # file: lib/database_connection.rb
 
-def self.connect
-  # If the environment variable (set by Heroku)
-  # is present, use this to open the connection.
-  if ENV['https://data.heroku.com/datastores/72254897-2276-40fb-9675-c0019e1908b0'] != nil
-    @connection = PG.connect(ENV['https://data.heroku.com/datastores/72254897-2276-40fb-9675-c0019e1908b0'])
-    return
-  end
+  def self.connect
+    # If the environment variable (set by Heroku)
+    # is present, use this to open the connection.
+    if ENV['https://data.heroku.com/datastores/72254897-2276-40fb-9675-c0019e1908b0'] != nil
+      @connection = PG.connect(ENV['https://data.heroku.com/datastores/72254897-2276-40fb-9675-c0019e1908b0'])
+      return
+    end
 
-  if ENV['ENV'] == 'test'
-    database_name = 'music_library_test'
-  else
-    database_name = 'music_library'
+    if ENV['ENV'] == 'test'
+      database_name = 'music_library_test'
+    else
+      database_name = 'music_library'
+    end
+    @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
   end
-  @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
-end
 
   # This method executes an SQL query 
   # on the database, providing some optional parameters
